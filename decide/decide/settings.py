f6e5861610f7ca5a105968af9f656c3fe9d5bda8
Jesús Roldán Cadena <72762597+jesrolcad@users.noreply.github.com>
2021-12-15 11:31:10 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,19 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://decidejrc.herokuapp.com'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -180,3 +192,8 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+NOSE_ARGS = [
+    '--with-xunit'
+]
+import django_heroku
+django_heroku.settings(locals(), test_runner=False)
